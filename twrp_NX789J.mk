604fa4bb21271fab31042777c2d719e3cb7b0ab4
reminon <reminon@gmail.com>
2026-03-10 14:53:41 -0500

    Update twrp_NX789J.mk

--- twrp_NX789J.mk
+++ twrp_NX789J.mk
@@ -4,17 +4,23 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/nubia/nx789j
+DEVICE_PATH := device/nubia/NX789J
 
 # Inherit from device.mk configuration
 $(call inherit-product, $(DEVICE_PATH)/device.mk)
 
-## Device identifier
+# Device identifier
 PRODUCT_DEVICE := NX789J
 PRODUCT_NAME := twrp_NX789J
 PRODUCT_BRAND := nubia
 PRODUCT_MANUFACTURER := nubia
 PRODUCT_MODEL := RedMagic 10 Pro
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := NX789J
+
+# Fingerprint
+BUILD_FINGERPRINT := nubia/NX789J-UN/NX789J:15/AQ3A.240812.002/RedMagicOS10.0.15_NX789J_GB:user/release-keys
 
 # Theme
 TW_STATUS_ICONS_ALIGN := center
